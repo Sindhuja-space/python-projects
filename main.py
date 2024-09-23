f8735273639838ee3f9eabf1483535654efe5e96
Sindhuja-space <sindhujapvt@gmail.com>
2024-09-23 07:25:43 +0530

    changes: user interface

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 print("Welcome to the Band Name Generator.")
 city =  input("What's the name of the city you grew up in?\n")
 pet_name = input("What's your pet's name?\n")
-band_name = f"Your Band Name could be {city} {pet_name}"
+band_name = f"Your Band Name could be: {city} {pet_name}"
 print(band_name)
