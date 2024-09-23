027c5b5a31c5b09fdc1d87952e9ba0c3384280b2
Sindhuja-space <sindhujapvt@gmail.com>
2024-09-23 07:17:01 +0530

    code: band name generator

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+city =  input("What's the name of the city you grew up in?")
+pet_name = input("What's your pet's name")
+band_name = f"Your Band Name could be {city} {pet_name}"
+print(band_name)
